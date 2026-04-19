716194e735e6f76ff5917a3aa295cff2b726d3e0
Jusalo24 <jusalo24@students.oamk.fi>
2026-04-19 19:31:44 +0300

    Ini Commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,44 +8,4 @@ plugins {
 }
 
 
-import java.util.Properties
-        import java.io.FileInputStream
 
-val localProperties = Properties()
-val localPropertiesFile = rootProject.file("local.properties")
-if (localPropertiesFile.exists()) {
-    localProperties.load(FileInputStream(localPropertiesFile))
-}
-
-android {
-    // ...
-
-    signingConfigs {
-        create("release") {
-            // Luetaan arvot local.properties -tiedostosta
-            storeFile = file(localProperties.getProperty("KEYSTORE_PATH") ?: "")
-            storePassword = localProperties.getProperty("KEYSTORE_PASSWORD") ?: ""
-            keyAlias = localProperties.getProperty("KEY_ALIAS") ?: ""
-            keyPassword = localProperties.getProperty("KEY_PASSWORD") ?: ""
-        }
-    }
-
-    buildTypes {
-        release {
-            // Käytä release-allekirjoitusta
-            signingConfig = signingConfigs.getByName("release")
-            // Pienennä koodia ProGuardilla
-            isMinifyEnabled = true
-            isShrinkResources = true
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-        debug {
-            // Debug-buildissa käytetään automaattisesti debug-avainta
-            applicationIdSuffix = ".debug"
-            versionNameSuffix = "-debug"
-        }
-    }
-}
